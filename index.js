033dd6c228a228595acc1e995c35f238cee0c081
Alister Norris <alister@alisternorris.com>
2017-02-27 01:25:59 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 (function (global) {
     'use strict';
 
-    var fileDialog = (...args) => {
+    const fileDialog = (...args) => {
         const input = document.createElement('input')
-        // Check if there config obj
+        
+        // Set config 
         if(typeof args[0] === 'object') {
             if(args[0].multiple === true) input.setAttribute('multiple','')
             if(args[0].accept !== undefined) input.setAttribute('accept',args[0].accept)
         } 
         input.setAttribute('type', 'file')
         
-        const evt = document.createEvent('MouseEvents');
-        evt.initMouseEvent('click', true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
-        input.dispatchEvent(evt);
-
+        // Simluate click event
+        const evt = document.createEvent('MouseEvents')
+        evt.initMouseEvent('click', true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null)
+        input.dispatchEvent(evt)
+        
+        // Return promise/callvack
         return new Promise((resolve, reject) => {
-            input.addEventListener('change', (e) => {
+            input.addEventListener('change', e => {
                 resolve(input.files)
                 const lastArg = args[args.length - 1]
                 if (typeof lastArg === "function") lastArg(input.files)
             })    
         })
-}
-
-
+    }
+    // Set either CommonJS/AMD/Global
     if (typeof define === 'function' && define.amd) {
         define(function () { return fileDialog; });
     } else if (typeof exports !== 'undefined') {
